5715931a0159f2a7ed5da0bd333340b8794389c6
Oseias Dev <oseiasbetodev@gmail.com>
2026-02-23 11:38:57 +0100

    Novos recursos

--- src/components/kools/Media.vue
+++ src/components/kools/Media.vue
@@ -1,21 +1,37 @@
 <script setup>
-import { defineProps } from 'vue';
-import PlayerVideo from '../UI/PlayerVideo.vue';
+import { defineProps, onMounted } from 'vue'
+import PlayerVideo from '../UI/PlayerVideo.vue'
+import GLightbox from 'glightbox'
+import 'glightbox/dist/css/glightbox.css'
 
 const props = defineProps({
   media: {
     type: Object,
     required: true
   }
-});
+})
+
+let lightbox = null
+
+onMounted(() => {
+  lightbox = GLightbox({
+    selector: '.glightbox', // seleciona elementos com esta classe
+    loop: true
+  })
+})
 </script>
 
 <template>
   <div class="mt-1.5 bg-secondary-500 overflow-hidden">
+    
+    <!-- Imagem com lightbox -->
     <div v-if="media.type === 'image'" class="w-full max-h-96 overflow-hidden">
-      <img v-lazy="media.url" alt="Kool media" class="w-full h-full m-0 object-cover">
+      <a :href="media.url" class="glightbox">
+        <img v-lazy="media.url" alt="Kool media" class="w-full h-full m-0 object-cover" />
+      </a>
     </div>
+
+    <!-- Vídeo: mantém PlayerVideo -->
     <PlayerVideo v-if="media.type === 'video'" :video="media" />
   </div>
-
 </template>
